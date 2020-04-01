816e23ee705277ca46fbc890f1b37a191fe7767e
Iris Dumeur <dum031@bracewell-login.cm.hpc>
2020-04-01 12:31:31 +1100

    before pull

--- install-esasnap.sh
+++ install-esasnap.sh
@@ -4,16 +4,10 @@ mkdir snap
 cd snap
 git clone https://github.com/senbox-org/snap-engine.git
 cd snap-engine
-mvn clean  -DskipTests=true install
+mvn clean   install
 
 cd ..
 git clone https://github.com/senbox-org/snap-desktop.git
 cd snap-desktop
-mvn clean  -DskipTests=true install
-
-cd ..
-git clone git clone https://github.com/senbox-org/s1tbx.git
-cd s1tbx
-mvn clean  -DskipTests=true install
-
+mvn clean  install
 
