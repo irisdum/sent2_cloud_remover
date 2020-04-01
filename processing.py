81d63ab3c3602859425ab1fa1e5f55c970a87046
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-02 10:54:55 +1100

    graph process

---
+++ processing.py
@@ -0,0 +1,14 @@
+# File which contains all the processing functions , mostly using snap
+
+
+def combine_image(list_path):
+    """Given the path to multiple images create a mosaic"""
+    pass
+
+def compute_the_intersection(image_path,zone_path):
+    """:param image_path : path to the image
+    :param path to the zone geometry"""
+
+
+def orthorectification(image_path,output_path):
+    pass
