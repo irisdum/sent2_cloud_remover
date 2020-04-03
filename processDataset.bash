910034e79ff6d65ac1c9f35377950951412de648
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-03 12:22:26 +1100

    Xml Graph working

--- processDataset.bash
+++ processDataset.bash
@@ -53,12 +53,12 @@ for F in $(ls -1d "${sourceDirectory}"/S1*.SAFE); do
   echo "$F"
   sourceFile="$(realpath "$F")"
   targetFile="${targetDirectory}/${targetFilePrefix}_$(removeExtension "$(basename ${F})").dim"
-  file_name_vv="vv_${targetDirectory}/${targetFilePrefix}_$(removeExtension "$(basename ${F})")_prepro"
-  file_name_vh="vh_${targetDirectory}/${targetFilePrefix}_$(removeExtension "$(basename ${F})")_prepro"
-  ${gptPath} ${graphXmlPath} -e -p "${parameterFilePath}"  -Pfile="vv_$(removeExtension "$(basename ${F})")" -PsourceBand=Amplitude_VV -t ${targetFile} ${sourceFile}
-  ${gptPath} ${graphXmlPath} -e -p "${parameterFilePath}"  -Pfile="vh_$(removeExtension "$(basename ${F})")" -PsourceBand=Amplitude_VH  -t ${targetFile} ${sourceFile}
-  gdalinfo "vv_$(removeExtension "$(basename ${F})").tif"
-  gdalinfo "vv_$(removeExtension "$(basename ${F})").tif"
+  file_name_vv="${targetDirectory}/${targetFilePrefix}_$(removeExtension "$(basename ${F})")_prepro"
+  file_name_vh="${targetDirectory}/${targetFilePrefix}_$(removeExtension "$(basename ${F})")_prepro"
+  ${gptPath} ${graphXmlPath} -e -p "${parameterFilePath}"  -Pfile="${targetDirectory}/vv_$(removeExtension "$(basename ${F})")_prepro" -PsourceBand=Amplitude_VV -t ${targetFile} ${sourceFile}
+  ${gptPath} ${graphXmlPath} -e -p "${parameterFilePath}"  -Pfile="${targetDirectory}/vh_$(removeExtension "$(basename ${F})")_prepro" -PsourceBand=Amplitude_VH  -t ${targetFile} ${sourceFile}
+  gdalinfo "${targetDirectory}/vv_$(removeExtension "$(basename ${F})")_prepro.tif"
+  gdalinfo "${targetDirectory}/vv_$(removeExtension "$(basename ${F})")_prepro.tif"
 done
 
 ####test if the image works
