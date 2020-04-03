0b6bef987f28b909ad19b2621d4de2260ba7f107
Iris Dumeur <dum031@bracewell-login.cm.hpc>
2020-04-03 11:41:16 +1100

    Before adding new xml

--- processDataset.bash
+++ processDataset.bash
@@ -10,8 +10,8 @@
 #export PATH=~/progs/snap/bin:$PATH
 export PATH=/home/dum031/snap/bin:$PATH
 gptPath="gpt -e"
-export JAVA_OPTS="-Xmx4096m -XX:CompressedClassSpaceSize=256m"
-export _JAVA_OPTIONS="-Xmx4096m -XX:CompressedClassSpaceSize=256m"
+export JAVA_OPTS="-Xmx8192m -XX:CompressedClassSpaceSize=256m"
+export _JAVA_OPTIONS="-Xmx8192m -XX:CompressedClassSpaceSize=256m"
 ############################################
 # Command line handling
 ############################################
