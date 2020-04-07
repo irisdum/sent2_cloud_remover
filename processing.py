eafc3d23896a4c39b17c1cc4ae561af89fe19965
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-07 18:06:52 +1000

    build datatser

--- processing.py
+++ processing.py
@@ -110,7 +110,7 @@ def _argparser():
 
 def create_tiling_hierarchy(input_dir,output_dir):
     for t in [0,1]:
-        create_safe_directory(input_dir + TEMPORARY_DIR[t])
+        create_safe_directory(input_dir + TEMPORARY_DIR)
     for cst in [XDIR,LABEL_DIR]:
         create_safe_directory(output_dir)
         create_safe_directory(output_dir+cst)
