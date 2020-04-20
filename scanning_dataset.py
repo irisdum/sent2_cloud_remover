d67a6798fb8725a4e22aa1f4f334a63fdc0ecd66
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-20 12:42:36 +1000

    Correct cloud mask ratserize

--- scanning_dataset.py
+++ scanning_dataset.py
@@ -1,13 +1,15 @@
 # Python file to scan the dataset and remove the non regular tile
 import glob
 import argparse
+from osgeo import gdal
 
 def is_no_data(raster):
     """Given a raster check if there are no data:
     :returns bool"""
-    pass
+    for
+    srcband.GetNoDataValue()
 
-def is_s2_cloud(s2_raster):
+def is_s2_cloud(s2_raster_array):
     """Given a sentinel 2 raster check if the cloud mask band contains no_data
     :returns bool """
     pass
@@ -22,6 +24,23 @@ def get_all_tiles_path(path_sent_dir):
     l= glob.glob("{}**/*.tif".format(path_sent_dir),recursive=True)
     assert len(l)>0, "No image found in {}".format(path_sent_dir)
     return l
+
+def is_conform(path_tile):
+    raster=gdal.Open(path_tile)
+    raster_array=raster.ReadAsArray()
+    assert raster_array.shape[0] in [2,5], "Wrong tile shape {} should be 3 or 5 bands".format(raster.shape)
+    if raster_array.shape[0]==5: # check sentinel 2 conformity
+        if is_s2_cloud(raster_array):
+            return False
+    if
+
+
+
+
+def main(path_sent_dir):
+    list_tiles=get_all_tiles_path(path_sent_dir)
+    for path_tile in list_tiles:
+
 
 
 def _argparser():
