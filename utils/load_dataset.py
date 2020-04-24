09003146765b698ac654ead30c153fb3b83a5803
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-24 18:10:04 +1000

    Rename model layers

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -109,8 +109,8 @@ def load_from_dir(path_dir, image_shape):
 
 def save_images(images, dir_path,ite=0):
     print(images.shape)
-    if len(images.shape ) >3:
+    if len(images.shape) >3:
         for i in range(images.shape[0]):
-            np.save(images[i, :, :, :], "{}image_{}_ite{}.npy".format(dir_path, i,ite))
+            np.save( "{}image_{}_ite{}.npy".format(dir_path, i,ite),images[i, :, :, :])
     else:
         np.save(images, "{}image_ite{}.npy".format(dir_path,ite))
