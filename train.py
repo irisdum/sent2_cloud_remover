995e47252f001b97d2ba50cec6ede79a041a1071
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-24 14:03:26 +1000

    Train the model

--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 #file to train the GAN
 from models import GAN
 from ruamel import yaml
+import tensorflow as tf
 
 def main(path_train,path_model):
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
 
-    model=GAN.GAN(open_yaml(path_train),open_yaml(path_model))
-
+        gan=GAN.GAN(open_yaml(path_train),open_yaml(path_model),sess)
+        gan.build_model()
+        gan.train()
+        
 def open_yaml(path_yaml):
     with open(path_yaml) as f:
         return yaml.load(f)
