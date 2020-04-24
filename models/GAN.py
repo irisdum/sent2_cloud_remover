16a27bb5390d76a801968af1db804656c44d8911
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-24 14:00:02 +1000

    Train the model

--- models/GAN.py
+++ models/GAN.py
@@ -1,19 +1,25 @@
-# This script is inspired by https://github.com/eriklindernoren/Keras-GAN/blob/master/gan/gan.py
+# This script is inspired by https://github.com/hwalsuklee/tensorflow-generative-model-collections/blob/master/GAN.py
+import time
+
 import tensorflow as tf
 from tensorflow import keras
 
-from tensorflow.keras.layers import Input, Dense, Add, Reshape, Flatten, Dropout, BatchNormalization, Conv2D, ReLU, add,ZeroPadding2D
+from tensorflow.keras.layers import Input, Dense, Add, Reshape, Flatten, Dropout, BatchNormalization, Conv2D, ReLU, add, \
+    ZeroPadding2D
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import Adam
 from constant.model_constant import CHANNEL, LEARNING_RATE, BETA_1, LOGDIR
-
+from utils.load_dataset import load_data, save_images
 import numpy as np
+from tensorflow_gan.python.losses.losses_impl import modified_discriminator_loss,modified_generator_loss
 from ruamel import yaml
+import os
 
 
 class GAN():
-    def __init__(self, train_yaml, model_yaml):
+    def __init__(self, train_yaml, model_yaml, sess):
         """:param train_yaml,model_yaml two dictionnaries"""
+        self.model_dir = train_yaml["model_dir"]
         print(train_yaml)
         print(model_yaml)
         self.img_rows = 256
@@ -23,63 +29,66 @@ class GAN():
         print(type(train_yaml["lr"]))
         optimizer = Adam(train_yaml["lr"], train_yaml["beta1"])
         # Build and compile the discriminator
-        self.discriminator = self.build_discriminator(model_yaml)
+        # self.discriminator = self.build_discriminator(model_yaml)
         # self.discriminator.compile(loss='binary_crossentropy',
         #                            optimizer=optimizer,
         #                            metrics=['accuracy'])
 
         # Build the generator
-        self.generator = self.build_generator(model_yaml)
+        # self.z = self.build_generator(model_yaml,)
+        self.log_dir = train_yaml["logdir"]
+        self.model_name=model_yaml["model_name"]
+        self.epoch = train_yaml["epoch"]
+        self.batch_size = train_yaml["batch_size"]
+        self.checkpoint_dir = train_yaml["checkpoint_dir"]
+        self.sess = sess
+        self.learning_rate = train_yaml["lr"]
+        self.fact_g_lr=train_yaml["fact_g_lr"]
+        self.beta1 = train_yaml["beta1"]
+        self.data_X, self.data_y = load_data(train_yaml["train_directory"])
+        self.num_batches = self.data_X.shape[0] // self.batch_size
+        self.model_yaml=model_yaml
+        self.saving_step=train_yaml["saving_step"]
+        # test
+        self.sample_num = train_yaml["n_train_image_saved"]  # number of generated images to be saved
+        self.result_dir=train_yaml["result_dir"]
 
-        # ##TODO the generator has as in input S1, S2, O1 and generate O2, work on the input
-        # # The generator takes noise as input and generates imgs
-        # z = Input(shape=(self.latent_dim,))
-        # img = self.generator(z)
-        #
-        # # For the combined model we will only train the generator
-        # self.discriminator.trainable = False  # TODO get what is the role of that !!!
-        #
-        # # The discriminator takes generated images as input and determines validity
-        # validity = self.discriminator(img)
-        #
-        # # The combined model  (stacked generator and discriminator)
-        # # Trains the generator to fool the discriminator
-        # self.combined = Model(z, validity)
-        # self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
-        #
+
         # ## Create the tensorboard logdir
-        # tensorboard_callback = keras.callbacks.TensorBoard(log_dir=train_yaml[
-        #     "logdir"])  # TODO change the name of the logdir make a function which gives a name to the train
+        tensorboard_callback = keras.callbacks.TensorBoard(log_dir=train_yaml[
+             "logdir"])
 
-    def build_discriminator(self,model_yaml,print_summary=True):
-        if model_yaml["d_activation"]=="lrelu":
-            d_activation= lambda x: tf.keras.activations.relu(x, alpha=model_yaml["lrelu_alpha"])
+    def discriminator(self, discri_input, model_yaml, print_summary=True, reuse=False):
+
+        if model_yaml["d_activation"] == "lrelu":
+            d_activation = lambda x: tf.keras.activations.relu(x, alpha=model_yaml["lrelu_alpha"])
         else:
-            d_activation= model_yaml["d_activation"]
-        discri_input=tf.keras.Input(shape=tuple(model_yaml["d_input_shape"]))
-        #layer 1
-        x=ZeroPadding2D(
-            padding=(1, 1))(discri_input)
-        x=Conv2D(64,4,padding="valid",activation=d_activation,strides=(2,2))(x)
-        #layer 2
-        x = ZeroPadding2D(padding=(1, 1))(x)
-        x = Conv2D(128, 4, padding="valid", activation=d_activation,strides=(2,2))(x)
-        #layer 3
-        x = ZeroPadding2D(padding=(1, 1))(x)
-        x = Conv2D(256, 4, padding="valid", activation=d_activation,strides=(2,2))(x)
-        # layer 4
-        x = ZeroPadding2D(padding=(1, 1))(x)
-        x = Conv2D(512, 4, padding="valid", activation=d_activation, strides=(1, 1))(x)
-        # layer 3
-        x = ZeroPadding2D(padding=(1, 1))(x)
-        x = Conv2D(1, 4, padding="valid", activation=d_activation, strides=(1, 1))(x)
-        print(x)
-        model=Model(discri_input,x,name="GAN_discriminator")
-        if print_summary:
-            model.summary()
-        return model
+            d_activation = model_yaml["d_activation"]
+        with tf.variable_scope("discriminator", reuse=reuse):
+            # discri_input=tf.keras.Input(shape=tuple(model_yaml["d_input_shape"]))
+            # layer 1
+            x = ZeroPadding2D(
+                padding=(1, 1))(discri_input)
+            x = Conv2D(64, 4, padding="valid", activation=d_activation, strides=(2, 2))(x)
+            # layer 2
+            x = ZeroPadding2D(padding=(1, 1))(x)
+            x = Conv2D(128, 4, padding="valid", activation=d_activation, strides=(2, 2))(x)
+            # layer 3
+            x = ZeroPadding2D(padding=(1, 1))(x)
+            x = Conv2D(256, 4, padding="valid", activation=d_activation, strides=(2, 2))(x)
+            # layer 4
+            x = ZeroPadding2D(padding=(1, 1))(x)
+            x = Conv2D(512, 4, padding="valid", activation=d_activation, strides=(1, 1))(x)
+            # layer 3
+            x = ZeroPadding2D(padding=(1, 1))(x)
+            x = Conv2D(1, 4, padding="valid", activation=d_activation, strides=(1, 1))(x)
 
-    def build_generator(self, model_yaml, is_training=True, print_summary=True):
+        if print_summary:
+            model = Model(discri_input, x, name="GAN_discriminator")
+            model.summary()
+        return x
+
+    def generator(self, img_input, model_yaml, is_training=True, print_summary=True, reuse=False):
 
         def build_resnet_block(input):
             """Define the ResNet block"""
@@ -90,33 +99,165 @@ class GAN():
             x = Dropout(rate=model_yaml["do_rate"])(x)
             x = BatchNormalization(momentum=model_yaml["bn_momentum"], trainable=is_training)(x)
             x = Add()([x, input])
-            x=ReLU()(x)
+            x = ReLU()(x)
             return x
 
-        if model_yaml["last_activation"]=="tanh":
+        if model_yaml["last_activation"] == "tanh":
             print("use tanh keras")
-            last_activ= lambda x: tf.keras.activations.tanh(x)
+            last_activ = lambda x: tf.keras.activations.tanh(x)
         else:
-            last_activ=model_yaml["last_activation"]
+            last_activ = model_yaml["last_activation"]
 
-        img_input = tf.keras.Input(shape=tuple(model_yaml["input_shape"]))
-        x=img_input
-        for i, param_lay in enumerate(model_yaml["param_before_resnet"]):  # build the blocks before the Resnet Blocks
-            x = Conv2D(param_lay[0], param_lay[1], strides=tuple(model_yaml["stride"]), padding=model_yaml["padding"],
-                       activation="relu")(x)
+        # img_input = tf.keras.Input(shape=tuple(model_yaml["input_shape"]))
+        with tf.variable_scope("generator", reuse=reuse):
+            x = img_input
+            for i, param_lay in enumerate(
+                    model_yaml["param_before_resnet"]):  # build the blocks before the Resnet Blocks
+                x = Conv2D(param_lay[0], param_lay[1], strides=tuple(model_yaml["stride"]),
+                           padding=model_yaml["padding"],
+                           activation="relu")(x)
 
-        for j in range(model_yaml["nb_resnet_blocs"]):  # add the Resnet blocks
-            x = build_resnet_block(x)
-        for i, param_lay in enumerate(model_yaml["param_after_resnet"]):
-            x=Conv2D(param_lay[0], param_lay[1], strides=tuple(model_yaml["stride"]), padding=model_yaml["padding"],
-                             activation="relu")(x)
-        # The last layer
-        x=Conv2D(model_yaml["last_layer"][0], model_yaml["last_layer"][1], strides=tuple(model_yaml["stride"]),
-                         padding=model_yaml["padding"],
-                         activation=last_activ)(x)
-        print("last layer gene", x)
-        print(type(img_input))
-        model=Model(img_input, x, name='GAN_generator')
+            for j in range(model_yaml["nb_resnet_blocs"]):  # add the Resnet blocks
+                x = build_resnet_block(x)
+            for i, param_lay in enumerate(model_yaml["param_after_resnet"]):
+                x = Conv2D(param_lay[0], param_lay[1], strides=tuple(model_yaml["stride"]),
+                           padding=model_yaml["padding"],
+                           activation="relu")(x)
+            # The last layer
+            x = Conv2D(model_yaml["last_layer"][0], model_yaml["last_layer"][1], strides=tuple(model_yaml["stride"]),
+                       padding=model_yaml["padding"],
+                       activation=last_activ)(x)
+            # print("last layer gene", x)
+            # print(type(img_input))
         if print_summary:
+            model = Model(img_input, x, name='GAN_generator')
             model.summary()
-        return model
+        return x
+
+    def build_model(self):
+
+        #The input in the model graph
+        self.g_input=tf.keras.backend.placeholder(shape=(self.batch_size,self.data_X.shape[1],self.data_X.shape[2],self.data_X.shape[3])) #the input of the label of the generator
+        #the Ground truth images
+        self.gt_images=tf.keras.backend.placeholder(shape=tuple([self.batch_size]+self.model_yaml["d_input_shape"]))
+
+        #the loss function
+        G=self.generator(self.g_input,self.model_yaml,is_training=True,print_summary=False,reuse=False)
+        D_input_real=np.append(self.gt_images,self.gt_images,axis=-1)  #input in the discriminator correspond to a pair of s2 images
+        D_input_fake=np.append(self.gt_images,G,axis=-1) #Input correpsond to the pair of images : Ground truth and synthetized image from the generator
+
+        self.d_loss=modified_discriminator_loss(D_input_real, D_input_fake, add_summaries=True)
+        # THE GENERATOR LOSS
+        discri_output=self.discriminator(D_input_fake,self.model_yaml,print_summary=False)
+        self.g_loss=modified_generator_loss(discri_output, add_summaries=True)
+
+        # divide trainable variables into a group for D and a group for G
+        t_vars = tf.trainable_variables()
+        d_vars = [var for var in t_vars if 'd_' in var.name]
+        g_vars = [var for var in t_vars if 'g_' in var.name]
+        print(d_vars,g_vars)
+
+        # optimizers
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            self.d_optim = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1) \
+                .minimize(self.d_loss, var_list=d_vars)
+            self.g_optim = tf.train.AdamOptimizer(self.learning_rate * self.fact_g_lr, beta1=self.beta1) \
+                .minimize(self.g_loss, var_list=g_vars)
+
+        # for test
+        self.fake_images = self.generator(self.g_input,self.model_yaml,print_summary=False, is_training=False, reuse=True)
+
+    def train(self):
+
+        # initialize all variables
+        tf.global_variables_initializer().run()
+        # graph inputs for visualize training results
+        self.sample_z = self.data_X[0,:,:,:]
+
+
+        # saver to save model
+        self.saver = tf.train.Saver()
+
+        # summary writer
+        self.writer = tf.summary.FileWriter(self.log_dir + '/' + self.model_name, self.sess.graph)
+
+        # restore check-point if it exits
+        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+        if could_load:
+            start_epoch = (int)(checkpoint_counter / self.num_batches)
+            start_batch_id = checkpoint_counter - start_epoch * self.num_batches
+            counter = checkpoint_counter
+            print(" [*] Load SUCCESS")
+        else:
+            start_epoch = 0
+            start_batch_id = 0
+            counter = 1
+            print(" [!] Load failed...")
+
+        # loop for epoch
+        start_time = time.time()
+        for epoch in range(start_epoch, self.epoch):
+
+            # get batch data
+            for idx in range(start_batch_id, self.num_batches):
+                batch_input = self.data_X[idx * self.batch_size:(idx + 1) * self.batch_size]
+                batch_gt=self.data_y[idx * self.batch_size:(idx + 1) * self.batch_size]
+                # update D network
+                _, summary_str, d_loss = self.sess.run([self.d_optim, self.d_loss],
+                                                       feed_dict={self.g_input: batch_input, self.gt_images: batch_gt})
+
+                # update G network
+                _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_loss],
+                                                       feed_dict={self.g_input: batch_input})
+                self.writer.add_summary(summary_str, counter)
+                # display training status
+                counter += 1
+                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                      % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss))
+
+                # save training results for every 300 steps
+                if np.mod(counter, self.saving_step) == 0:
+                    samples = self.sess.run(self.fake_images, feed_dict={self.g_input: self.sample_z})
+                    tot_num_samples = min(self.sample_num, self.batch_size)
+                    manifold_h = int(np.floor(np.sqrt(tot_num_samples)))
+                    manifold_w = int(np.floor(np.sqrt(tot_num_samples)))
+                    save_images(samples[:manifold_h * manifold_w, :, :, :],self.result_dir,ite=counter)
+
+            # After an epoch, start_batch_id is set to zero
+            # non-zero value is only for the first epoch after loading pre-trained model
+            start_batch_id = 0
+            # save model
+            self.save(counter)
+
+        # save model for final step
+        self.save(counter)
+
+    def load(self, checkpoint_dir):
+        if checkpoint_dir is None:
+            return False, 0
+
+        import re
+        print(" [*] Reading checkpoints...")
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir, self.model_name) #TODO modify that
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            counter = int(next(re.finditer("(\d+)(?!.*\d)",ckpt_name)).group(0))
+            print(" [*] Success to read {}".format(ckpt_name))
+            return True, counter
+        else:
+            print(" [*] Failed to find a checkpoint")
+            return False, 0
+
+    def save(self, step):
+        checkpoint_dir = os.path.join(self.model_dir, self.model_name)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, self.model_name + '.model'), global_step=step)
+
+    def visualize_results(self, epoch):
+        pass
--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -1,9 +1,10 @@
 # All the functions used to load the tiles
 from processing import create_safe_directory
 import os
-from constant.gee_constant import XDIR, LABEL_DIR, DICT_ORGA,DICT_SHAPE
+from constant.gee_constant import XDIR, LABEL_DIR, DICT_ORGA, DICT_SHAPE
 from constant.model_constant import TRAINING_DIR
 from scanning_dataset import find_path
+from utils.display_image import find_image_indir
 from utils.display_image import convert_array
 from osgeo import gdal
 import numpy as np
@@ -18,7 +19,6 @@ def make_dataset_hierarchy(path_dataset):
         os.mkdir(path_dataset + sub_dir + LABEL_DIR)
 
 
-
 def tiff_2_array(path_tif):
     assert os
     raster = gdal.Open(path_tif)
@@ -31,11 +31,11 @@ def modify_array(raster_array):
     It converts the data type
     The normalization is also taken applied"""
 
-    if raster_array.shape[-1]>4: #there is the cloud mask which is the 5th band, should be removed
-        #TODO think of a better way more generalized to remove the cloud band
-        raster_array=raster_array[:4,:]
-    raster_array=np.moveaxis(raster_array, 0, -1) #transform to channel last
-    #TODO normalization
+    if raster_array.shape[-1] > 4:  # there is the cloud mask which is the 5th band, should be removed
+        # TODO think of a better way more generalized to remove the cloud band
+        raster_array = raster_array[:4, :]
+    raster_array = np.moveaxis(raster_array, 0, -1)  # transform to channel last
+    # TODO normalization
     return convert_array(raster_array)
 
 
@@ -49,31 +49,67 @@ def create_input(image_id, input_dir, output_dir):
     :return:
     """
     assert input_dir[-1] == "/", "Wrong path should end with / not {}".format(input_dir)
-    for name_dir in DICT_ORGA:#there will be one tile created one for x one for y (label)
-        final_array=np.zeros(DICT_SHAPE[name_dir])
-        count_dim=0
-        for i,sent in enumerate(DICT_ORGA[name_dir]):
+    for name_dir in DICT_ORGA:  # there will be one tile created one for x one for y (label)
+        final_array = np.zeros(DICT_SHAPE[name_dir])
+        count_dim = 0
+        for i, sent in enumerate(DICT_ORGA[name_dir]):
             image_path = find_path(input_dir + name_dir + sent, image_id)
-            raster_array = modify_array(tiff_2_array(image_path)) # channel last s*s*channel
-            assert raster_array.shape[0]==final_array.shape[0], "Wrong dimension between final_array {} and raster_array {}".format(final_array.shape,raster_array.shape)
-            final_array[:,:,count_dim:count_dim+raster_array.shape[-1]]=raster_array #we add the array into the final_array
-            count_dim+=raster_array.shape[-1]
-        np.save("{}{}.npy".format(output_dir+name_dir,image_id[:-4]),final_array)
+            raster_array = modify_array(tiff_2_array(image_path))  # channel last s*s*channel
+            assert raster_array.shape[0] == final_array.shape[
+                0], "Wrong dimension between final_array {} and raster_array {}".format(final_array.shape,
+                                                                                        raster_array.shape)
+            final_array[:, :,
+            count_dim:count_dim + raster_array.shape[-1]] = raster_array  # we add the array into the final_array
+            count_dim += raster_array.shape[-1]
+        np.save("{}{}.npy".format(output_dir + name_dir, image_id[:-4]), final_array)
 
 
-def prepare_tiles_from_id(list_id,input_dir, output_dir):
+def prepare_tiles_from_id(list_id, input_dir, output_dir):
     """Given a list of id and a directory create and save 2 npy array with corresponds to tile on the input of the model and its label
     :param input_dir is the path to the directory which contains dataX dan label directory"""
     for image_id in list_id:
-        create_input(image_id,input_dir,output_dir)
+        create_input(image_id, input_dir, output_dir)
 
 
-def create_input_dataset(dict_tiles,input_dir,output_dir):
+def create_input_dataset(dict_tiles, input_dir, output_dir):
     """dict tiles is type {"train/": ["01_02.tif",...],"val/":[list of id],"test/":[list of id] }
     input_dir should contains label/ and DataX
     output_dir will contains the npy tile created"""
     make_dataset_hierarchy(output_dir)
     for sub_dir in dict_tiles:
-        assert sub_dir in TRAINING_DIR, "Issue name directory is {} but should be in {}".format(sub_dir,TRAINING_DIR)
-        prepare_tiles_from_id(dict_tiles[sub_dir],input_dir,output_dir+sub_dir)
+        assert sub_dir in TRAINING_DIR, "Issue name directory is {} but should be in {}".format(sub_dir, TRAINING_DIR)
+        prepare_tiles_from_id(dict_tiles[sub_dir], input_dir, output_dir + sub_dir)
 
+
+def load_data(path_directory, x_shape=None, label_shape=None, normalization=None):
+    """:param path_directory : path to the directory (train,test or val) which contains two directory dataX and label """
+    # TODO work on a normailzation, centering of the data !!
+    if x_shape is None:
+        x_shape = DICT_SHAPE[XDIR]
+    if label_shape is None:
+        label_shape = DICT_SHAPE[LABEL_DIR]
+    assert x_shape[0] == label_shape[0], "Label and data does not have the same dimension label {} data {}".format(
+        label_shape, x_shape)
+    dataX = load_from_dir(path_directory + XDIR, x_shape)
+    data_label = load_from_dir(path_directory + LABEL_DIR, label_shape)
+    assert data_label.shape[0] == dataX.shape[0], "Not the same nber of label {} and dataX {}".format(label_shape,
+                                                                                                      x_shape)
+    return dataX, data_label
+
+
+def load_from_dir(path_dir, image_shape):
+    path_tile = find_image_indir(path_dir, "npy")
+    batch_x_shape = (len(path_tile), image_shape[0], image_shape[1], image_shape[-1])
+    data_array = np.zeros(batch_x_shape)
+    for i, tile in enumerate(path_tile):
+        data_array[i, :, :, :] = np.load(tile)
+    return data_array
+
+
+def save_images(images, dir_path,ite=0):
+    print(images.shape)
+    if len(images.shape > 3):
+        for i in images.shape[0]:
+            np.save(images[i, :, :, :], "{}image_{}_ite{}.npy".format(dir_path, i,ite))
+    else:
+        np.save(images, "{}image_ite{}.npy".format(dir_path,ite))
