d6d153d9c796694167a67228a4eabbccb818beda
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-04-24 17:59:10 +1000

    Rename model layers

--- models/GAN.py
+++ models/GAN.py
@@ -207,13 +207,13 @@ class GAN():
                 batch_input = self.data_X[idx * self.batch_size:(idx + 1) * self.batch_size]
                 print("batch_input ite {} shape {} ".format(idx,batch_input.shape))
                 batch_gt=self.data_y[idx * self.batch_size:(idx + 1) * self.batch_size]
-                #print("GT",batch_gt.shape)
+                print("GT",batch_gt.shape)
                 # update D network
                 summary_str, d_loss = self.sess.run([self.d_optim, self.d_loss],
                                                        feed_dict={self.g_input: batch_input, self.gt_images: batch_gt})
                 #self.writer.add_summary(d_loss, counter)
                 # update G network
-                #print("Before G run ", self.g_input,batch_input.shape)
+                print("Before G run ", self.g_input,batch_input.shape)
                 summary_str,g_loss = self.sess.run([self.g_optim, self.g_loss],
                                                        feed_dict={self.g_input: batch_input,self.gt_images:batch_gt})
 
