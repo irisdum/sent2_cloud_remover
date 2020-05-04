6f8a1aaf1f064dfe2fdabfa09247f66ebb101135
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-05-04 14:02:50 +1000

    add wasserstein loss

--- models/losses.py
+++ models/losses.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 
 #loss from https://github.com/tensorflow/gan/blob/master/tensorflow_gan/python/losses/losses_impl.py
 
+def _to_float(tensor):
+  return tf.cast(tensor, tf.float32)
+
+
 def minimax_discriminator_loss(
     discriminator_real_outputs,
     discriminator_gen_outputs,
@@ -266,9 +270,115 @@ def noisy_discriminator_loss(D_output_real,D_output_fake,noise_real,noise_fake):
 
 
 def load_loss(loss_name):
-    assert loss_name in ["total_generator_loss","noisy_discriminator_loss"],"Loss {} undefined add it to load loss".format(loss_name)
+    assert loss_name in ["total_generator_loss","noisy_discriminator_loss","wasserstein_discriminator_loss","wasserstein_generator_loss"],"Loss {} undefined add it to load loss".format(loss_name)
     if loss_name=="total_generator_loss":
         return total_generatot_loss
+    if loss_name=="wasserstein_generator_loss":
+        return wasserstein_generator_loss
+    if loss_name=="wasserstein_discriminator_loss":
+        return wasserstein_discriminator_loss
     elif loss_name=="noisy_discriminator_loss":
         return noisy_discriminator_loss
 
+
+def wasserstein_generator_loss(
+    gt_images,
+    generated_images,
+    discriminator_gen_outputs,
+    val_lambda=0,
+    weights=1.0,
+    scope=None,
+    loss_collection=tf.compat.v1.GraphKeys.LOSSES,
+    reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+  """Wasserstein generator loss for GANs.
+  See `Wasserstein GAN` (https://arxiv.org/abs/1701.07875) for more details.
+  Args:
+    discriminator_gen_outputs: Discriminator output on generated data. Expected
+      to be in the range of (-inf, inf).
+    weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `discriminator_gen_outputs`, and must be broadcastable to
+      `discriminator_gen_outputs` (i.e., all dimensions must be either `1`, or
+      the same as the corresponding dimension).
+    scope: The scope for the operations performed in computing the loss.
+    loss_collection: collection to which this loss will be added.
+    reduction: A `tf.losses.Reduction` to apply to loss.
+    add_summaries: Whether or not to add detailed summaries for the loss.
+  Returns:
+    A loss Tensor. The shape depends on `reduction`.
+  """
+  with tf.compat.v1.name_scope(scope, 'generator_wasserstein_loss',
+                               (discriminator_gen_outputs, weights)) as scope:
+    discriminator_gen_outputs = _to_float(discriminator_gen_outputs)
+
+    loss = - discriminator_gen_outputs
+    loss = tf.compat.v1.losses.compute_weighted_loss(loss, weights, scope,
+                                                     loss_collection, reduction)
+
+    if add_summaries:
+      tf.compat.v1.summary.scalar('generator_wass_loss', loss)
+
+  return loss,calc_cycle_loss(gt_images,generated_images,val_lambda)
+
+
+def wasserstein_discriminator_loss(
+    discriminator_real_outputs,
+    discriminator_gen_outputs,
+    noise1=0,
+    noise_2=0,
+    real_weights=1.0,
+    generated_weights=1.0,
+    scope=None,
+    loss_collection=tf.compat.v1.GraphKeys.LOSSES,
+    reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+  """Wasserstein discriminator loss for GANs.
+  See `Wasserstein GAN` (https://arxiv.org/abs/1701.07875) for more details.
+  Args:
+    discriminator_real_outputs: Discriminator output on real data.
+    discriminator_gen_outputs: Discriminator output on generated data. Expected
+      to be in the range of (-inf, inf).
+    real_weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `discriminator_real_outputs`, and must be broadcastable to
+      `discriminator_real_outputs` (i.e., all dimensions must be either `1`, or
+      the same as the corresponding dimension).
+    generated_weights: Same as `real_weights`, but for
+      `discriminator_gen_outputs`.
+    scope: The scope for the operations performed in computing the loss.
+    loss_collection: collection to which this loss will be added.
+    reduction: A `tf.losses.Reduction` to apply to loss.
+    add_summaries: Whether or not to add summaries for the loss.
+  Returns:
+    A loss Tensor. The shape depends on `reduction`.
+  """
+  with tf.compat.v1.name_scope(
+      scope, 'discriminator_wasserstein_loss',
+      (discriminator_real_outputs, discriminator_gen_outputs, real_weights,
+       generated_weights)) as scope:
+    discriminator_real_outputs = _to_float(discriminator_real_outputs)
+    discriminator_gen_outputs = _to_float(discriminator_gen_outputs)
+    discriminator_real_outputs.shape.assert_is_compatible_with(
+        discriminator_gen_outputs.shape)
+
+    loss_on_generated = tf.compat.v1.losses.compute_weighted_loss(
+        discriminator_gen_outputs,
+        generated_weights,
+        scope,
+        loss_collection=None,
+        reduction=reduction)
+    loss_on_real = tf.compat.v1.losses.compute_weighted_loss(
+        discriminator_real_outputs,
+        real_weights,
+        scope,
+        loss_collection=None,
+        reduction=reduction)
+    loss = loss_on_generated - loss_on_real
+    tf.compat.v1.losses.add_loss(loss, loss_collection)
+
+    if add_summaries:
+      tf.compat.v1.summary.scalar('discriminator_gen_wass_loss',
+                                  loss_on_generated)
+      tf.compat.v1.summary.scalar('discriminator_real_wass_loss', loss_on_real)
+      tf.compat.v1.summary.scalar('discriminator_wass_loss', loss)
+
+  return loss_on_generated,-loss_on_real
