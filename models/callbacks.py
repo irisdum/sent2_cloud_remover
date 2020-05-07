74467700076ce9568fddaed5525673b56ea1986a
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-05-07 13:44:34 +1000

    tracking training

--- models/callbacks.py
+++ models/callbacks.py
@@ -6,12 +6,7 @@ def write_log(callback, names, logs, batch_no):
     for name, value in zip(names, logs):
         summary = tf.Summary()
         summary_value = summary.value.add()
-        if PREFIX_IM in name: #FOR AN IMAGE:
-            summary_value.image=value
-        elif PREFIX_HIST in name:
-            summary_value.histo=value
-        else:
-            summary_value.simple_value = value
+        summary_value.simple_value = value
         summary_value.tag = name
         callback.writer.add_summary(summary, batch_no)
         callback.writer.flush()
