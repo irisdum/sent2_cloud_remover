0f2986d15d8bd8912a6509180488d1baafd6dbdc
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-05-14 18:05:04 +1000

    prepro sent2 all 4 bands together

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ wkt_file=confs/train_kangaroo_wkt2.txt
 graph_xml_sent1=snap-confs/calibrate_sent1_v2.xml
 source_directory=/datastore/dum031/data/dataset2/
 target_directory=${source_directory}/prepro2/
-
-geojson_2_wkt: ${wkt_file}
+graph_xml_sent2=snap-confs/calibrate_sent2.xml
+geojson_2_wkt:
 		@echo "We convert ${geojson_file} into ${wkt_file}"
 		@python convert_geojson.py --input ${geojson_file} --output ${wkt_file}
 
@@ -12,3 +12,7 @@ convert_sent1:
 		@echo "Starting preprocessing Sentinel 1"
 		./new_processDatasetSent1.bash ${graph_xml_sent1} snap-confs/orbite.properties ${source_directory}date1 ${target_directory}date1 ${wkt_file}
 		./new_processDatasetSent1.bash ${graph_xml_sent1} snap-confs/orbite.properties ${source_directory}date2 ${target_directory}date2 ${wkt_file}
+convert_sent2:
+	@echo "Starting preprocessing Sentinel 2"
+	./new_processDatasetSent1.bash ${graph_xml_sent2} snap-confs/orbite.properties ${source_directory}date1 ${target_directory}date1 ${wkt_file}
+	./new_processDatasetSent1.bash ${graph_xml_sent2} snap-confs/orbite.properties ${source_directory}date2 ${target_directory}date2 ${wkt_file}
---
+++ new_preprocessDatasetSent2.bash
@@ -0,0 +1,71 @@
+#!/bin/bash
+# enable next line for debugging purpose
+# set -x
+
+############################################
+# User Configuration
+############################################
+
+# adapt this path to your needs
+#export PATH=~/progs/snap/bin:$PATH
+export PATH=$PATH:/datastore/dum031/code/snap/bin
+gptPath="gpt -e"
+export JAVA_OPTS="-Xmx8192m -XX:CompressedClassSpaceSize=256m"
+export _JAVA_OPTIONS="-Xmx8192m -XX:CompressedClassSpaceSize=256m"
+
+############################################
+# Command line handling
+############################################
+
+# first parameter is a path to the graph xml
+graphXmlPath="$1"
+
+# second parameter is a path to a parameter file
+parameterFilePath="$2"
+
+# use third parameter for path to source products
+sourceDirectory="$3"
+
+# use fourth parameter for path to target products
+targetDirectory="$4"
+
+
+#the sixth parameter is the path to the WKT subset .txt file
+wktFile="$5"
+
+
+
+############################################
+# Helper functions
+############################################
+removeExtension() {
+    file="$1"
+    echo "$(echo "$file" | sed -r 's/\.[^\.]*$//')"
+}
+
+
+# Create the target directory
+mkdir -p "${targetDirectory}"
+
+# the d option limits the elemeents to loop over to directories. Remove it, if you want to use files.
+for F in $(ls -1d "${sourceDirectory}"/S2*.SAFE); do
+  # echo "$F"
+  sourceFile="${F}"
+  # During the preprocess we split the images on smaller tiles
+  # shellcheck disable=SC1035
+  echo "${sourceFile}"
+  echo "${wktFile}"
+  i=1
+  while IFS= read -r poly; do
+    targetFilePrefix="process1_${i}"
+    targetFile="${targetDirectory}/${targetFilePrefix}_$(removeExtension "$(basename ${F})").dim"
+    ${gptPath} ${graphXmlPath} -e -p "${parameterFilePath}"  -Pfile="${targetDirectory}/$(removeExtension "$(basename ${F})")_prepro_${i}" -PBands="B2,B3,B4,B8" -Pgeometry="${poly}" -t  ${targetFile} ${sourceFile}
+    i=$((i+1))
+  done<"${wktFile}"
+  echo "${targetDirectory}cloud_mask_$(removeExtension "$(basename ${F})").tif"
+  temp_cloud="temp_cloud_mask_$(removeExtension "$(basename ${F})").tif"
+  fmask_sentinel2Stacked.py -o "${targetDirectory}${temp_cloud}" --safedir ${F} -v --mincloudsize 30 --cloudprobthreshold 5
+  gdal_translate "${targetDirectory}${temp_cloud}" -ot Uint16 -tr 10 10 "${targetDirectory}cm_$(removeExtension "$(basename ${F})").tif"
+
+done
+
