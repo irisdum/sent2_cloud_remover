552bd37e429958d8df1a3b7dfafdff0866e8431e
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-05-26 18:52:05 +1000

    Use the stats from csv

--- tests/test_read_stats.py
+++ tests/test_read_stats.py
@@ -37,7 +37,7 @@ def main(test_name,dataset_dir,input_dataset):
         print("[TEST] load_data function")
         dataX, data_label=load_data(input_dataset, x_shape=None, label_shape=None, normalization=True, dict_band_X=None,
                   dict_band_label=None, dict_rescale_type=None, dir_csv=dataset_dir)
-        print(np.mean(dataX[0,:,:,4],np.mean(data_label[0,:,:,0])))
+        print(np.mean(dataX[0,:,:,4]),np.mean(data_label[0,:,:,0]))
 if __name__ == '__main__':
     args=_argparser()
     main(args.test_name,args.dataset_dir,args.input_dataset)
