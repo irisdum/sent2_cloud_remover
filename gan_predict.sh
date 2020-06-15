dc1ed28373a76065b03a3061776f8eb5227f777b
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2020-06-15 18:35:40 +1000

    Modif cs

--- gan_predict.sh
+++ gan_predict.sh
@@ -18,9 +18,11 @@ dataset="$4"
 
 pref="$5"
 
+path_csv="$6"
+
 module load miniconda3/4.3.13
 unset PYTHONHOME
 unset PYTHONPATH
 source activate /datastore/dum031/envs/env_tf_gpu
-python predict.py --model_path ${model_path}   --tr_nber ${train_nber} --dataset ${dataset} --pref ${pref} --weights ${weight}
+python predict.py --model_path ${model_path}   --tr_nber ${train_nber} --dataset ${dataset} --pref ${pref} --weights ${weight} --path_csv ${path_csv}
 
