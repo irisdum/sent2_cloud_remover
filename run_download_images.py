77df0bafc9f94c027ff13cd32b9713c9658442fb
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2020-11-25 16:45:47 +0100

    clean dwnd code

--- run_download_images.py
+++ run_download_images.py
@@ -5,6 +5,8 @@ import ee
 import argparse
 import sys
 
+from typing import Tuple
+
 from utils.download_images import download_all, create_download_dir
 from find_image import get_filter_collection, list_image_name, opt_filter, gjson_2_eegeom, eedate_2_string
 from utils.fp_functions import sub_collection_tiles, extract_fp, check_clip_area, zone_in_images, get_biggest_s1_image
@@ -31,12 +33,13 @@ def _argparser():
                              "sentinel 2 image between bd1 and bd2 with less clouds")
     parser.add_argument("--optparam1", type=str, default=None, help="optional parameters to filter the data of "
                                                                     "sentinel 1 ")
-    parser.add_argument("--ccp", type=int, default=20, help="Percentage of cloud allowed in the image, not really used anymore the roi_cpp defined in the constant file is more restrictive")
+    parser.add_argument("--ccp", type=int, default=20,
+                        help="Percentage of cloud allowed in the image, not really used anymore the roi_cpp defined in the constant file is more restrictive")
     parser.add_argument("--save", default=True, help="wether or not we are going to store the images")
     parser.add_argument("--output_path", default=True, help="where the image preprocess and ordered are going "
                                                             "to be stored")
-    parser.add_argument("--s2_t0",default=None,help="If known name of the S2 image at t0 to download")
-    parser.add_argument("--s2_t1",default=None,help="If known name of the S2 image at t0 to download")
+    parser.add_argument("--s2_t0", default=None, help="If known name of the S2 image at t0 to download")
+    parser.add_argument("--s2_t1", default=None, help="If known name of the S2 image at t0 to download")
 
     return parser.parse_args()
 
@@ -57,7 +60,8 @@ def collection_lenght(collection):
     return collection.toList(1000).length().getInfo()
 
 
-def sent_image_search(date_t, zone, sent, optparam1, i, opt_search):
+def sent_image_search(date_t: ee.Date, zone: ee.Geometry, sent: int, optparam1: dict, i: int, opt_search: str) -> Tuple[
+    int, ee.ImageCollection]:
     """
 
     Args:
@@ -111,22 +115,23 @@ def get_sentinel1_image(date_t, zone, optparam1, opt_search="both", sent=1):
     print("Test day +- {} from {}".format(0, date_t.format().getInfo()))
     i = 1
     total_len, dayli_collection = sent_image_search(date_t, zone, sent, optparam1, i, opt_search)
-    all_found,final_image_collection= get_biggest_s1_image(zone,dayli_collection) #boolean wether or not all the good image has been selected
+    all_found, final_image_collection = get_biggest_s1_image(zone,
+                                                             dayli_collection)  # boolean wether or not all the good image has been selected
     print(type(zone), zone.getInfo())
-    #final_collection=dayli_collection
-    while all_found is False: # iterate until a sentinel 1 image is found
+    # final_collection=dayli_collection
+    while all_found is False:  # iterate until a sentinel 1 image is found
         print("Test day +- {} from {}".format(i, date_t.format().getInfo()))
         i += 1
         total_len, dayli_collection = sent_image_search(date_t, zone, sent, optparam1, i, opt_search)
-        #final_collection=final_collection.merge(dayli_collection)
-        all_found,final_image_collection=get_biggest_s1_image(zone,dayli_collection)
+        # final_collection=final_collection.merge(dayli_collection)
+        all_found, final_image_collection = get_biggest_s1_image(zone, dayli_collection)
     print("Number of image found of sent 1  found {} at {} days from sentinel 2 ".format(total_len, i))
     final_list = list_image_name(final_image_collection, sent)
     assert len(final_list) > 0, "Pb the list is empty {}".format(final_list)
     list_subcol_sent1 = sub_collection_tiles(final_image_collection, zone, sent)  # Get subcollections list
-    list_name_sent1=[]
-    list_date_sent1=[]
-    #assert len(list_subcol_sent1)==1,"We should only find 1 Sentinel 1 image "
+    list_name_sent1 = []
+    list_date_sent1 = []
+    # assert len(list_subcol_sent1)==1,"We should only find 1 Sentinel 1 image "
     for sub_list in list_subcol_sent1:
         list_name_sent1 += list_image_name(sub_list, sent)
         list_date_sent1 += [sub_list.first().date() for image in list_image_name(sub_list, sent)]
@@ -148,6 +153,7 @@ def clip_on_geometry(geometry):
     Returns: a function which corresponds to clipping an ee.Image along the input geometry
 
     """
+
     def clip0(image):
         return image.clip(geometry)
 
@@ -168,7 +174,7 @@ def sent2_filter_clouds(collection, sent2criteria, ccp, zone):
         Three argument which corresponds to a string name of the image of the first image of the collection, an ee.Date which
         is the date of the first the Image of the output collection, the footprint of the first image of the collection
     """
-   #TODO check the use of this function, used the new one which claculate the ccp on the area !!!
+    # TODO check the use of this function, used the new one which claculate the ccp on the area !!!
     print("before clipping length collection = {}".format(collection.toList(100).length().getInfo()))
 
     collection_zone = collection.map(clip_on_geometry(zone))
@@ -214,7 +220,7 @@ def extract_name_date_first(collection, sent):
     return name, date_coll, zone  # TODO take care of the zone format read
 
 
-def download_sent2_sent1(bd, ed, zone, sent2criteria, optparam1, ccp,name_s2):
+def download_sent2_sent1(bd, ed, zone, sent2criteria, optparam1, ccp, name_s2):
     """
     Args:
         bd: string, begin date
@@ -233,10 +239,10 @@ def download_sent2_sent1(bd, ed, zone, sent2criteria, optparam1, ccp,name_s2):
     dict_image_dwnld1 = {}
     dict_image_dwnld2 = {}
     # Extract the Image collection of sentinel 2 between the range dates
-    print("With {} - {} looking for {}".format(bd,ed,name_s2))
+    print("With {} - {} looking for {}".format(bd, ed, name_s2))
     global_collection_sent2_t1 = get_filter_collection(bd, ed, zone, 2, opt_param={
-        "ccp": ccp},name_s2=name_s2)
-    if name_s2 is None: #Only for automatic search, should be improved and tested
+        "ccp": ccp}, name_s2=name_s2)
+    if name_s2 is None:  # Only for automatic search, should be improved and tested
         # Extract the List of subcollection with one subcollection = image between the range date
         # at one special tile
         print(type(global_collection_sent2_t1))
@@ -250,28 +256,31 @@ def download_sent2_sent1(bd, ed, zone, sent2criteria, optparam1, ccp,name_s2):
             name, date1_sent2_subcol, zone_sent2 = sent2_filter_clouds(sub_col, sent2criteria,
                                                                        ccp, zone)  # returns the image with less clouds
             # on the specific zone which is the intersection of the two
-            #print("zone {}".format(type(zone)))
-            #print("zone  sent2 {}".format(type(zone)))
+            # print("zone {}".format(type(zone)))
+            # print("zone  sent2 {}".format(type(zone)))
             # print("Zone {}".format(zone_sent2.coordinates().getInfo()))
             # list_name_sent2 += [name]  # save the name of the sent2 image at t1 to download
             # we extract the footprint of sentinel 2 : we extract now all the sentinel 1 images which can reproduce this
             # image
             dict_image_dwnld2.update({name: eedate_2_string(date1_sent2_subcol)})
-            list_name_sent1, list_date_sent1 = get_sentinel1_image(date1_sent2_subcol, zone_sent2.intersection(zone,0.001), optparam1, "both")
+            list_name_sent1, list_date_sent1 = get_sentinel1_image(date1_sent2_subcol,
+                                                                   zone_sent2.intersection(zone, 0.001), optparam1,
+                                                                   "both")
             dict_image_dwnld1.update(
                 dict(zip(list_name_sent1, [eedate_2_string(date) for date in list_date_sent1])))
-           # list_sent1_sent2_name += list_name_sent2 + list_name_sent1  # collect all the names
+        # list_sent1_sent2_name += list_name_sent2 + list_name_sent1  # collect all the names
     else:
-        name, date1_sent2_subcol, zone_sent2= extract_name_date_first(global_collection_sent2_t1,2)
+        name, date1_sent2_subcol, zone_sent2 = extract_name_date_first(global_collection_sent2_t1, 2)
 
         dict_image_dwnld2.update({name: eedate_2_string(date1_sent2_subcol)})
-        list_name_sent1, list_date_sent1 = get_sentinel1_image(date1_sent2_subcol,zone_sent2.intersection(zone,0.001), optparam1, "both")
+        list_name_sent1, list_date_sent1 = get_sentinel1_image(date1_sent2_subcol, zone_sent2.intersection(zone, 0.001),
+                                                               optparam1, "both")
         dict_image_dwnld1.update(
             dict(zip(list_name_sent1, [eedate_2_string(date) for date in list_date_sent1])))
     return dict_image_dwnld1, dict_image_dwnld2
 
 
-def main(bd, ed, bd2, ed2, path_zone, sent2criteria, optparam1, ccp, save, output_path, s2_t0=None,s2_t1=None):
+def main(bd, ed, bd2, ed2, path_zone, sent2criteria, optparam1, ccp, save, output_path, s2_t0=None, s2_t1=None):
     """
 
     Args:
@@ -292,7 +301,6 @@ def main(bd, ed, bd2, ed2, path_zone, sent2criteria, optparam1, ccp, save, outpu
 
     """
 
-
     assert create_download_dir(output_path), "Download directory has not been well created"
 
     if optparam1 is None:
@@ -304,26 +312,27 @@ def main(bd, ed, bd2, ed2, path_zone, sent2criteria, optparam1, ccp, save, outpu
     # print({"ccp": ccp})
     zone_sent2_init = gjson_2_eegeom(path_zone)
     print(type(zone_sent2_init))
-    dic_name_t1_sent1, dic_name_t1_sent2 = download_sent2_sent1(bd, ed, zone_sent2_init, sent2criteria, optparam1, ccp,s2_t0)
+    dic_name_t1_sent1, dic_name_t1_sent2 = download_sent2_sent1(bd, ed, zone_sent2_init, sent2criteria, optparam1, ccp,
+                                                                s2_t0)
     print("{} {}".format(bd2, ed2))
     print(dic_name_t1_sent1, dic_name_t1_sent2, )
     dic_name_t2_sent1, dic_name_t2_sent2 = download_sent2_sent1(bd2, ed2, zone_sent2_init, sent2criteria, optparam1,
-                                                                ccp,s2_t1)
-    print(dic_name_t2_sent1, dic_name_t2_sent2 )
+                                                                ccp, s2_t1)
+    print(dic_name_t2_sent1, dic_name_t2_sent2)
 
     if save:
-        download_all(dic_name_t2_sent1, sent=1,output_path= output_path+DIR_T[1],opt=OPT_DWND_IMAGE)
-        download_all(dic_name_t1_sent1, sent=1, output_path=output_path+DIR_T[0],opt=OPT_DWND_IMAGE)
-        download_all(dic_name_t2_sent2, sent=2,output_path= output_path+DIR_T[1],opt=OPT_DWND_IMAGE)
-        download_all(dic_name_t1_sent2, sent=2,output_path= output_path+DIR_T[0],opt=OPT_DWND_IMAGE)
+        download_all(dic_name_t2_sent1, sent=1, output_path=output_path + DIR_T[1], opt=OPT_DWND_IMAGE)
+        download_all(dic_name_t1_sent1, sent=1, output_path=output_path + DIR_T[0], opt=OPT_DWND_IMAGE)
+        download_all(dic_name_t2_sent2, sent=2, output_path=output_path + DIR_T[1], opt=OPT_DWND_IMAGE)
+        download_all(dic_name_t1_sent2, sent=2, output_path=output_path + DIR_T[0], opt=OPT_DWND_IMAGE)
 
     else:
         return True
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     sys.path.append("./")
     args = _argparser()
     main(args.bd1, args.ed1, args.bd2, args.ed2, args.zone, args.sent2criteria, args.optparam1, int(args.ccp),
          args.save,
-         args.output_path,args.s2_t0,args.s2_t1)
+         args.output_path, args.s2_t0, args.s2_t1)
