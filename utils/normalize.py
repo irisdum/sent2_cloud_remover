d7ed11123edb4b55c84e944448d79a5b627fb772
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2020-12-10 15:12:54 +0100

    Correction norm

--- utils/normalize.py
+++ utils/normalize.py
@@ -10,7 +10,7 @@ from constant.gee_constant import DICT_BAND_X, DICT_BAND_LABEL, DICT_METHOD, DIC
     CONVERTOR
 from sklearn.preprocessing import StandardScaler, RobustScaler
 from constant.processing_constant import DICT_RESCALE, DICT_GROUP_BAND_LABEL, DICT_GROUP_BAND_X, S1_BANDS, S2_BANDS, \
-    DICT_RESCALE_TYPE, DICT_SCALER, FACTEUR_STD_S2
+    DICT_RESCALE_TYPE, DICT_SCALER, FACTEUR_STD_S2, FACTEUR_STD_S1
 from utils.image_find_tbx import extract_tile_id, find_csv
 import matplotlib.pyplot as plt
 import numpy as np
@@ -228,7 +228,7 @@ def conv1D_dim(tuple_dim):
 def rescale_array(batch_X: np.array, batch_label, dict_group_band_X=None, dict_group_band_label=None,
                   dict_rescale_type=None,
                   s1_log=True, dict_scale=None, invert=False, s2_bands=S2_BANDS, s1_bands=S1_BANDS,
-                  fact_scale=FACTEUR_STD_S2) -> Tuple[np.array, np.array, dict]:
+                  fact_scale2=FACTEUR_STD_S2,fact_scale1=FACTEUR_STD_S1) -> Tuple[np.array, np.array, dict]:
     """
 
     Args:
@@ -281,7 +281,7 @@ def rescale_array(batch_X: np.array, batch_label, dict_group_band_X=None, dict_g
         data_flatten_sar_band = data_sar_band.reshape(
             conv1D_dim(data_sar_band.shape))  # Modify into 2D array as required for sklearn
         output_data, sar_scale = sklearn_scale(dict_rescale_type[group_bands], data_flatten_sar_band,
-                                               scaler=dict_scale[group_bands], fact_scale=1)
+                                               scaler=dict_scale[group_bands], fact_scale=fact_scale1)
         rescaled_batch_X[:, :, :, dict_group_band_X[group_bands]] = output_data.reshape(init_shape)  # reshape it
         dict_scaler.update({group_bands: sar_scale})
     for group_bands in s2_bands:
@@ -293,7 +293,7 @@ def rescale_array(batch_X: np.array, batch_label, dict_group_band_X=None, dict_g
 
         flat_rescale_data, scale_s2 = sklearn_scale(dict_rescale_type[group_bands], data_flatten,
                                                     scaler=dict_scale[group_bands], invert=invert,
-                                                    fact_scale=fact_scale)
+                                                    fact_scale=fact_scale2)
         rescale_global_data = flat_rescale_data.reshape(global_shape)
         # print("rescale_global_shape {} sub {} fit in {} & label {}".format(rescale_global_data.shape,
         #                                                         rescale_global_data[:m , :, :, :].shape,
