c58ca6bed77086892e579c147cce212a2cbed1e9
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2021-01-12 10:47:44 +0100

    add bash script

---
+++ grid5000_train.sh
@@ -0,0 +1,15 @@
+# first parameter is a path to the graph xml
+model_path="$1"
+
+# second parameter is a path to a parameter file
+train_path="$2"
+
+# path to the dataset that we should copy
+dataset_path="$3"
+
+source /home/idumeur/miniconda3/etc/profile.d/conda.sh
+conda activate training_env
+
+cp dataset_path -R /tmp
+
+python train.py --model_path ${model_path} --train_path ${train_path}
