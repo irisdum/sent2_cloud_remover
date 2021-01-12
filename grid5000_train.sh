807c97b486ca2b61e1f785014ea60516065537fc
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2021-01-12 11:17:54 +0100

    Modif batch_script

--- grid5000_train.sh
+++ grid5000_train.sh
@@ -1,7 +1,16 @@
+# first parameter is a path to the graph xml
+model_path="$1"
+
+# second parameter is a path to a parameter file
+train_path="$2"
+
+dataset_path="$3"
 
 source /home/idumeur/miniconda3/etc/profile.d/conda.sh
 conda activate training_env
 
-cp /srv/osirim/idumeur/data/dataset6/prepro1/input_large_dataset/ -R /tmp
 
-python train.py --model_path GAN_confs/model_0.yaml --train_path GAN_confs/train5.yaml
+
+cp ${dataset_path} -R /tmp
+
+python train.py --model_path ${model_path} --train_path ${train_path} --mgpu true
