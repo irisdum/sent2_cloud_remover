bc45f719751ad31046badf28aea78d1d67eb725e
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2021-01-13 14:23:29 +0100

    Modif cuddn install

--- grid5000_train.sh
+++ grid5000_train.sh
@@ -3,7 +3,9 @@
 source /home/idumeur/miniconda3/etc/profile.d/conda.sh
 conda activate training_env
 
-
+export LD_LIBRARY_PATH=/home/idumeur/miniconda3/lib:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=/home/idumeur/miniconda3/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/idumeur/login/miniconda3/include:$CPLUS_INCLUDE_PATH
 
 cp /srv/osirim/idumeur/data/dataset6/prepro1/input_large_dataset.zip  /tmp
 unzip tmp/input_large_dataset.zip
