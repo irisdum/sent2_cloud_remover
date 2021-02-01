43916e8d223b3014c8c96bcfcfaf19747e1ddf58
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2021-02-01 16:12:49 +0100

    bacth change detection correction

--- change_detection/detection.py
+++ change_detection/detection.py
@@ -4,6 +4,8 @@ from sklearn.decomposition import PCA
 from collections import Counter
 import warnings
 import cv2
+import pickle
+from utils.image_find_tbx import create_safe_directory
 
 
 def find_vector_set(diff_image: np.array, n_channel=1, kernel_dim=5):
@@ -99,10 +101,11 @@ def find_FVS(EVS, diff_image, mean_vec, kernel_dim, padding="symmetric"):
     return FVS
 
 
-def clustering(FVS, components, input_shape):
+def clustering(FVS, components, input_shape, kmeans=None):
     """
 
     Args:
+        kmeans:
         FVS: Feature vector space
         components: nber of cluster K in Kmeans
         input_shape: (N*N)
@@ -110,13 +113,14 @@ def clustering(FVS, components, input_shape):
     Returns:
 
     """
-    kmeans = KMeans(components, verbose=0)
-    kmeans.fit(FVS)
+    if kmeans is None:
+        kmeans = KMeans(components, verbose=0)
+        kmeans.fit(FVS)
     output = kmeans.predict(FVS)
     count = Counter(output)
     least_index = min(count, key=count.get)
-    change_map = np.reshape(output, (input_shape[0], input_shape[1]))
-    return least_index, change_map
+    change_map = np.reshape(output,input_shape)
+    return least_index, change_map,kmeans
 
 
 def map_detection(image1, image2, kernel_dim=4, n_components="full", k=2, padding="symmetric"):
@@ -150,7 +154,7 @@ def map_detection(image1, image2, kernel_dim=4, n_components="full", k=2, paddin
     print("We are using a symetric padding to add the missing dimension ")
 
     FVS = find_FVS(EVS, diff_image, mean_vec, kernel_dim, padding)
-    least_index, change_map = clustering(FVS, k, input_dim)
+    least_index, change_map,kmeans = clustering(FVS, k, input_dim)
     change_map[change_map == least_index] = 255
     change_map[change_map != 255] = 0
 
@@ -164,10 +168,14 @@ def map_detection(image1, image2, kernel_dim=4, n_components="full", k=2, paddin
 
     return diff_image, change_map, cleanChangeMap
 
-def ACP_on_batch(batch1,batch2, kernel_dim=4, n_components="full", k=2, padding="symmetric",save=False):
+
+def ACP_on_batch(batch1, batch2, kernel_dim=4, n_components=3, k=2, padding="symmetric",band=None, path_save=None,load_dir=None):
     """
     NOT FINISEHD DO NOT LOOK !
     Args:
+        load_dir:
+        path_save:
+        band: should be a list, by default 0 [0]
         batch1:
         batch2:
         kernel_dim:
@@ -179,53 +187,73 @@ def ACP_on_batch(batch1,batch2, kernel_dim=4, n_components="full", k=2, padding=
     Returns:
 
     """
-    list_vector_set=[]
+    if band is None:
+        band=[0]
+    list_vector_set = []
+    print("Batch size {}".format(batch1.shape[0]))
+    if load_dir is  None:
+        for i in range(batch1.shape[0]):  # TODO parallelize to improve the computation time if too slow
+            image1 = batch1[i, :, :, band]
+            image2 = batch2[i, :, :, band]
 
-    for i in range(batch1.shape[0]): #TODO parallelize to improve the computation time if too slow
-        image1=batch1[i,:,:,:]
-        image2=batch2[i,:,:,:]
-        diff_image = abs(image1 - image2)
-        input_dim = (diff_image.shape[1], diff_image.shape[2])
-        vector_set, mean_vec = find_vector_set(diff_image, n_channel=diff_image.shape[0], kernel_dim=kernel_dim)
-        list_vector_set+=[vector_set]
+            diff_image = abs(image1 - image2)
+            # print(diff_image.shape)
+            input_dim = (diff_image.shape[1], diff_image.shape[2])
+            vector_set, mean_vec = find_vector_set(diff_image, n_channel=diff_image.shape[0], kernel_dim=kernel_dim)
+            list_vector_set += [vector_set]
 
-    batch_vect_set=np.array(list_vector_set)
-    vector_set_dim=(list_vector_set[0].shape[0],list_vector_set[1].shape[0])
-    batch_vect_set=batch_vect_set.reshape((len(list_vector_set)*vector_set_dim[0],vector_set_dim[1])) # a way to concatenate all the data
-    batch_mean_vect=np.mean(batch_vect_set, axis=0)
+        batch_vect_set = np.array(list_vector_set)
+        print("batch vect set {}".format(batch_vect_set.shape))
+        vector_set_dim = (list_vector_set[0].shape[0], list_vector_set[0].shape[1])
+        print(vector_set_dim)
+        batch_vect_set = batch_vect_set.reshape(
+            (len(list_vector_set) * vector_set_dim[0], vector_set_dim[1]))  # a way to concatenate all the data
+        print("after reshape {}".format(batch_vect_set.shape))
 
-    #batch_vect_set has the features built for the whole input batches
-    pca = PCA(n_components=n_components)
-    pca.fit(batch_vect_set)
-    EVS = pca.components_
-    list_FVS=[]
+     # batch_vect_set has the features built for the whole input batches
+        pca = PCA(n_components=n_components)
+        pca.fit(batch_vect_set)
+        EVS = np.transpose(pca.components_)
+
+        batch_mean_vect = np.mean(batch_vect_set, axis=0)
+        kmeans=None
+    else:
+        EVS=np.load(load_dir+"EVS")
+        batch_mean_vect=np.load(load_dir+"mean_vect")
+        kmeans=pickle.load(open("{}kmeans.pkl".format(path_save), "rb"))
+    list_FVS = []
+
     for i in range(batch1.shape[0]):
-        image1 = batch1[i, :, :, :]
-        image2 = batch2[i, :, :, :]
+        image1 = batch1[i, :, :, band]
+        image2 = batch2[i, :, :, band]
         diff_image = abs(image1 - image2)
-        input_dim = (diff_image.shape[1], diff_image.shape[2])
+        # print("FVS diff im {}".format(diff_image.shape))
         FVS = find_FVS(EVS, diff_image, batch_mean_vect, kernel_dim, padding)
-        list_FVS+=[FVS]
-    batch_FVS=np.array(list_FVS)
-    input_fvs_dim=batch_FVS.shape
-    dim=list_FVS[0].shape
-    batch_FVS.reshape(len(list_FVS)*dim[0],dim[1])
-    least_index, change_map = clustering(batch_FVS, k, input_dim) #TODO think of clustering on the whole batch. Concatenate and then reshape into the different array
+        list_FVS += [FVS]
+    batch_FVS = np.array(list_FVS)
+    dim = list_FVS[0].shape
+    batch_FVS = batch_FVS.reshape(len(list_FVS) * dim[0], dim[1])
+    print("FVS shape{}".format(batch_FVS.shape))
+    shape_change_map = (batch1.shape[0], batch1.shape[1], batch1.shape[2])
+    least_index, change_map,kmeans = clustering(batch_FVS, k,
+                                                shape_change_map,kmeans=kmeans)  # TODO think of clustering on the whole batch. Concatenate and then reshape into the different array
     change_map[change_map == least_index] = 255
     change_map[change_map != 255] = 0
-    change_map.reshape(input_fvs_dim)
+    change_map.reshape(shape_change_map)
     kernel = np.asarray(((0, 0, 1, 0, 0),
                          (0, 1, 1, 1, 0),
                          (1, 1, 1, 1, 1),
                          (0, 1, 1, 1, 0),
                          (0, 0, 1, 0, 0)), dtype=np.uint8)
-    batch_clean_change_map=np.zeros(change_map.shape)
+    batch_clean_change_map = np.zeros(change_map.shape)
     for i in range(change_map.shape[0]):
-        change_map[i,:,:,:] = change_map[i,:,:,:].astype(np.uint8)
+        batch_clean_change_map[i, :, :] = cv2.erode(change_map[i, :, :].astype(np.uint8), kernel)
 
-        batch_clean_change_map[i,:,:,:] = cv2.erode(change_map, kernel)
-    if save:
-        pass #add saving ACP model
-        #save the K-means model
+    if path_save is not None: #TO SAVE THE MODEL
+        assert path_save[-1]=="/".format("Wrong path_save name should be a directory path {}".format(path_save))
+        create_safe_directory(path_save)
+        np.save(path_save+"EVS",EVS)
+        np.save(path_save+"mean_vect",batch_mean_vect)
+        pickle.dump(kmeans, open("{}kmeans.pkl".format(path_save), "wb"))
 
     return None, change_map, batch_clean_change_map
