3def3c17f3d1821ccfa4e37cbdd13347aa783a58
irisdum <iris.dumeur6@etu.univ-lorraine.fr>
2021-02-01 16:30:32 +0100

    bacth change detection correction

--- change_detection/detection.py
+++ change_detection/detection.py
@@ -218,8 +218,8 @@ def ACP_on_batch(batch1, batch2, kernel_dim=4, n_components=3, k=2, padding="sym
         batch_mean_vect = np.mean(batch_vect_set, axis=0)
         kmeans=None
     else:
-        EVS=np.load(load_dir+"EVS")
-        batch_mean_vect=np.load(load_dir+"mean_vect")
+        EVS=np.load(load_dir+"EVS.npy")
+        batch_mean_vect=np.load(load_dir+"mean_vect.npy")
         kmeans=pickle.load(open("{}kmeans.pkl".format(path_save), "rb"))
     list_FVS = []
 
